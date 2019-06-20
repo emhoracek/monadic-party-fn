1ffc45cacb1d890007fa3e0809de5449e9b80f75
Libby Horacek <libby@daydrea.me>
2019-06-20 14:24:22 +0200

    Remove rivet-autoimporter

--- migrations/rivet.hs
+++ migrations/rivet.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -F -pgmF rivet-autoimporter #-}
 module Main where
 
 import           Database.Rivet.Adaptor.PostgreSQL
 import qualified Database.Rivet.Main               as Rivet
 import           System.Environment
 
+import M20190620_initial_schema (migrate)
 
 main :: IO ()
 main = do args <- getArgs
@@ -16,4 +16,4 @@ main = do args <- getArgs
                  ["status"] -> Rivet.MigrateStatus
                  _ -> error "Usage: [executable] [devel|prod|...] [up|down|status]"
           adaptor <- setup id (ConnectInfo "localhost" 5432 "monadic_party_user" "111" "monadic_party_db")
-          Rivet.main adaptor mode migrations
+          Rivet.main adaptor mode [("M20190620_initial_schema", migrate)]
